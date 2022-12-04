34fe0fdfdf4068887aef033e8189fc5fd1dd0f09
Ashu0Singh <ashutosh.20bci7070@vitap.ac.in>
2022-12-05 00:06:15 +0530

    Status added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,51 +1,41 @@
 import './Login.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Wrapper from '../Wrapper/Wrapper'
-import {useEffect, useState} from 'react'
-import { checkResponse } from '../OpenAIApi';
-import { userCollectionRef } from '../../firebase/firebase';
-import { getDocs } from 'firebase/firestore';
+import { useState } from 'react'
+import Axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Login(){
 
-    const [classNames , setClassName] = useState(['','','']);
-    const [isLoading , setIsLoading] = useState('Submit')
-    const [input , setInput ] = useState({teamName:'',apiKey:'',secCode:''});
-    const [userData , setUserData] = useState([]);
-    useEffect( () => {
-        const getData = async () => {
-            console.log("checkpoint1");
-            const database = await getDocs(userCollectionRef);
-            database.docs.map(
-                doc => {
-                    const {team , security_code} = doc.data();
-                    setUserData(prevValue => [...prevValue,{teamName:team , secCode : security_code}]);
-            });
-            return database;
-        }
-        getData();
-    }, []);
-    
-    async function handleSubmit(){
+    const [classNames , setClassName] = useState(['is-valid','','','']);
+    const [isLoading , setIsLoading] = useState('Submit');
+    const [input , setInput ] = useState(
+    {
+        name:'RandomGuy',
+        team_name:'BLOODY_MARY',
+        api_key:'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',
+        security_code:'685617'
+    });
+    function handleSubmit(){
         setIsLoading('');
-        const checkTeam = userData.find(({teamName , secCode}) => {
-            if(teamName === input.teamName && secCode === input.secCode){
-                setClassName(prevValue => (['is-valid', prevValue[1],prevValue[2]]));
-                setClassName(prevValue => ([prevValue[0],prevValue[1],'is-valid']));
-                return true;
+        Axios.post(
+            "http://3.6.65.227:8080/api/login",
+            {
+                "name":`${input.name}`,
+                "team_name":`${input.team_name}`,
+                "api_key":`${input.api_key}`,
+                "security_code":`${input.security_code}`
             }
-            else{
-                setClassName(prevValue => (['is-invalid', prevValue[1],'is-invalid']));
+        ).then(({data:{isLoggedIn , message}, status}) => {
+            if(status === 200 && isLoggedIn){
+                setIsLoading("Submitted");
+                toast.success(message);
+            }else{
+                setIsLoading("Retry")
+                toast.error(message);
             }
         });
-        const checkApiKey = await checkResponse(input.apiKey , setClassName);
-        console.log(checkApiKey + "  " + checkTeam);
-        if(checkTeam && checkApiKey){
-                setIsLoading('Submitted');
-        }
-        else{
-            setIsLoading('Retry');
-        }
     }
 
 
@@ -61,12 +51,24 @@ export default function Login(){
                     <div className="form-floating">
                         <input 
                             type="text" 
-                            id='floatingInput' 
+                            id='floatingName' 
                             onChange={handleChange} 
                             className={`form-control inputFeilds ${classNames[0]}`} 
+                            placeholder="Vinay"
+                            name='name'
+                            value={input.name}
+                        />
+                        <label htmlFor="floatingName">Name</label>
+                    </div>
+                    <div className="form-floating">
+                        <input 
+                            type="text" 
+                            id='floatingInput' 
+                            onChange={handleChange} 
+                            className={`form-control inputFeilds ${classNames[1]}`} 
                             placeholder="Cute Capybara"
-                            name='teamName'
-                            value={input.teamName}
+                            name='team_name'
+                            value={input.team_name}
                         />
                         <label htmlFor="floatingInput">Team Name</label>
                     </div>
@@ -75,10 +77,10 @@ export default function Login(){
                             type="text" 
                             id="floatingAPI" 
                             onChange={handleChange} 
-                            className={`form-control inputFeilds ${classNames[1]}`} 
+                            className={`form-control inputFeilds ${classNames[2]}`} 
                             placeholder="xxxxxxxxxxxx"
-                            name='apiKey'
-                            value={input.apiKey}
+                            name='api_key'
+                            value={input.api_key}
                         />
                         <label htmlFor="floatingAPI">API Key</label>
                     </div>
@@ -87,18 +89,32 @@ export default function Login(){
                             type="password" 
                             id='floatingCode' 
                             onChange={handleChange} 
-                            className={`form-control inputFeilds ${classNames[2]}`} 
+                            className={`form-control inputFeilds ${classNames[3]}`} 
                             placeholder="234209"
-                            name='secCode'
-                            value={input.secCode}
+                            name='security_code'
+                            value={input.security_code}
                         />
                         <label htmlFor="floatingCode">Security Code</label>
                     </div>
                     <button className='button fs-50 extrabold fc-white' onClick={handleSubmit}>
-                        {isLoading === "" ? <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> : isLoading}
+                        {isLoading === "" ? 
+                        <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> 
+                        :isLoading}
                     </button>
                 </div>
             </div>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+            />
         </Wrapper>
     )
 }
