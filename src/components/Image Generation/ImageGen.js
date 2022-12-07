cb3604bc6148fabdd7c18d7b162daa306fbf004c
Ashu0Singh <ashutosh.20bci7070@vitap.ac.in>
2022-12-07 17:25:09 +0530

    Image Changes

--- src/components/Image Generation/ImageGen.js
+++ src/components/Image Generation/ImageGen.js
@@ -41,8 +41,6 @@ function GenerateImg() {
     .then(res => {
       console.log(res.data);
     });
-    
-    setInfo({ prompt: "", size: "", number: "", url: "" });
   };
 
   const generateImg = async () => {
@@ -85,24 +83,43 @@ function GenerateImg() {
   return (
     <Wrapper>
       <div className='flex-col borders'>
-        <h1 className='fc-white'>{' >Image Generation '}</h1>
-        <div className='flex-col fc-white content'>
+        <h1 className='fc-white title'>{' >Image Generation '}</h1>
+        <div className='flex-col fc-white imgGen'>
           <form onSubmit={handleSubmit}>
+
+
           <div class="form-floating">
-            <textarea 
+            <textarea
                 className="form-control inputFeilds" 
-                placeholder="Leave a comment here" 
-                id="floatingTextarea">
+                placeholder="Enter your prompt here" 
+                id="floatingTextarea"
+                name='prompt'
+                onChange={handleChange}
+            >
             </textarea>
             <label for="floatingTextarea">Comments</label>
           </div>
-            <div className='input-container full-width'>
-              <label htmlFor="">Prompt</label>
-              <textarea name="prompt" max-cols='20' maxRows='7' value={info.prompt} onChange={handleChange} />
-            </div>
+
+
+          <div className="form-floating">
+              <input 
+                  type="text" 
+                  id='floatingName' 
+                  onChange={handleChange} 
+                  className={`form-control inputFeilds`} 
+                  placeholder="Vinay"
+                  name='name'
+                  value={info.size}
+              />
+              <label htmlFor="floatingName">Name</label>
+          </div>
             
             <div className='input-container'>
               <label>Size</label>
+              <select class="form-select" value={info.size}>
+                <option value="1">1</option>
+                <option value="4">4</option>
+              </select>
               <input type='text' name='size' value={info.size} onChange={handleChange} />
             </div>
             
