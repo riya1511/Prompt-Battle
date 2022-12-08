9c16aea056144047c3e229c7768f72503de334e8
Ashu0Singh <ashutosh.20bci7070@vitap.ac.in>
2022-12-08 14:19:08 +0530

    Initial Change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Homepage from './components/Home/Homepage'
 import Poll from './components/Polling/Poll';
-import Login from './components/Login/Login'
 import { BrowserRouter as Router , Route , Routes} from 'react-router-dom'
 import GenerateImg from './components/Image Generation/ImageGen'
 function App() {
@@ -9,11 +8,10 @@ function App() {
     <Router className="App">
       <Routes>
         
-        <Route exact path="/Prompt-Battle/" element={< GenerateImg />} />
-        {/* <Route exact path="/Prompt-Battle/" element={<Homepage />} /> */}
+        <Route exact path="/Prompt-Battle/" element={<Homepage />} /> 
+        <Route exact path="/Prompt-Battle/GenerateImage" element={< GenerateImg />} />
         <Route exact path="/Prompt-Battle/Homepage" element={<Homepage />} />
         <Route exact path="/Prompt-Battle/Polling" element={<Poll />} />
-        <Route exact path="/Prompt-Battle/Login" element={<Login />} />
       </Routes>
     </Router>
   );
